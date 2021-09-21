83c7686ac32ec87d785fd0874196d50dbb98c6ec
anu_cecs_zl <u6766505@anu.edu.au>
2021-09-21 14:51:41 +1000

    Add files via upload

--- selected_cmp_data/segment_cmp_label.py
+++ selected_cmp_data/segment_cmp_label.py
@@ -1,29 +1,26 @@
 import torchvision
-import numpy
 import torch
 import segmentation_utils
 from PIL import Image
-import matplotlib.pyplot as plt
 import torchvision.transforms as transforms
-from torch.utils.data import DataLoader, Dataset
 import glob
-from datetime import datetime
 import cv2
 
 # set computation device
 device = torch.device('cpu')
-
+# create a transform function for data normalization
 transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406],
                          std=[0.229, 0.224, 0.225])
 ])
-
+# load the images and labels to the computation device
+# it's noted that the images should have same dimension with the labels.
 images = []
 for img in glob.glob("train_images/*.png"):
     images.append(Image.open(img))
-
-model = torchvision.models.segmentation.fcn_resnet101(num_classes=9).to(device)
+# load the saved model and set it in evaluation mode
+model = torchvision.models.segmentation.lraspp_mobilenet_v3_large(num_classes=9).to(device)
 model.load_state_dict(torch.load('saved_model.pt'))
 model.eval()
 
@@ -35,7 +32,8 @@ for image in images:
     outputs = model(transform(image).unsqueeze(0).to(device))
     # get the data from the `out` key
     outputs = outputs['out']
-
+    # save the overlaid images with predicted labels
     segmented_image = segmentation_utils.draw_segmentation_map(outputs)
     final_image = segmentation_utils.image_overlay(image, segmented_image)
-    cv2.imwrite(image.filename, final_image)
+    fp = 'segmentation_results/' + image.filename.split('\\')[1]
+    cv2.imwrite(fp, final_image)
